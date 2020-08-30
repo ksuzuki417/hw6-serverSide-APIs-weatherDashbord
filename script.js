1f07aea48e1712913a73531c025fcb3ce2cccb97
Keita Suzuki <ksuzuki417@gmail.com>
2020-08-29 20:52:02 -0500

    5 day forecast modified

--- script.js
+++ script.js
@@ -17,14 +17,14 @@ $("#searchBtn").on("click", function getWeather(){
         //city name
         let name = $("<div>");
         name.text(response.name);
-        $("#currentWeather").append(name);
+        $("#cityName").append(name);
 
         //today's date
         var today = moment().format("dddd, MMMM Do YYYY");
         console.log(today);
         let date = $("<div>");
-        date.text(today)
-        $("#currentWeather").append(date)
+        date.innerHTML = today
+        $("#todaysDate").append(date.innerHTML)
         
         //weather icon
         let icon = response.weather[0].icon;
@@ -32,23 +32,24 @@ $("#searchBtn").on("click", function getWeather(){
         console.log(iconURL);
         let weatherIcon = $("<div>");
         weatherIcon.html("<img src='" + iconURL + "'>");
-        $("#currentWeather").append(weatherIcon);
+        $("#weatherIcon").append(weatherIcon);
+        console.log(weatherIcon);
         
         //temperature
         let temperature = $("<div>");
         var fahrenheit = kelvin2F(response.main.temp);
-        temperature.text(fahrenheit);
-        $("#currentWeather").append(temperature);
+        temperature.innerHTML = "Temperature: " + fahrenheit + "F";
+        $("#temperature").append(temperature.innerHTML);
         
         //humidity
         let humidity = $("<div>");
-        humidity.text(response.main.humidity);
-        $("#currentWeather").append(humidity);
+        humidity.innerHTML = "Humidity: " + response.main.humidity;
+        $("#humidity").append(humidity.innerHTML);
         
         //wind speed
         let windSpeed = $("<div>");
-        windSpeed.text(response.wind.speed);
-        $("#currentWeather").append(windSpeed);
+        windSpeed.innerHTML = "Wind Speed: " + response.wind.speed;
+        $("#windSpeed").append(windSpeed.innerHTML);
 
         //latitude element
         let latEl = response.coord.lat;
@@ -69,8 +70,8 @@ $("#searchBtn").on("click", function getWeather(){
 
         //UV index
         let uvIndex = $("<div>");
-        uvIndex.text(uvResponse.value);
-        $("#currentWeather").append(uvIndex);
+        uvIndex.innerHTML = "UV Index: " + uvResponse.value;
+        $("#uvIndex").append(uvIndex.innerHTML);
         })
     })
 
@@ -85,9 +86,9 @@ $("#searchBtn").on("click", function getWeather(){
 
         //For Day 1
         //date
-        let date1 = $("<div>");
+        var date1 = $("<div>");
         date1.text(fcstResponse.list[7].dt_txt);
-        $("#fiveDayForecast").append(date1);
+        $("#forecastDate1").append(date1);
         
         //weather icon
         let day1Icon = fcstResponse.list[7].weather[0].icon;
@@ -95,23 +96,22 @@ $("#searchBtn").on("click", function getWeather(){
         console.log(day1IconURL);
         let day1WeatherIcon = $("<div>");
         day1WeatherIcon.html("<img src='" + day1IconURL + "'>");
-        $("#fiveDayForecast").append(day1WeatherIcon);
+        $("#forecastIcon1").append(day1WeatherIcon);
         //temperature
-        let day1Temperature = $("<div>");
-        var day1Fahrenheit = kelvin2F(fcstResponse.list[7].main.temp);
-        day1Temperature.text(day1Fahrenheit);
-        $("#fiveDayForecast").append(day1Temperature);
-
+        let day1Temp = $("<div>");
+        var day1F = kelvin2F(fcstResponse.list[7].main.temp);
+        day1Temp.innerHTML = "Temperature: " + day1F + "F";
+        $("#forecastTemp1").append(day1Temp.innerHTML);
         //humidity
         let day1Humidity = $("<div>");
-        day1Humidity.text(fcstResponse.list[7].main.humidity);
-        $("#fiveDayForecast").append(day1Humidity);
+        day1Humidity.innerHTML = "Humidity: " + fcstResponse.list[7].main.humidity;
+        $("#forecastHumidity1").append(day1Humidity.innerHTML);
 
         //For Day 2
          //date
          let date2 = $("<div>");
          date2.text(fcstResponse.list[15].dt_txt);
-         $("#fiveDayForecast").append(date2);
+         $("#forecastDate2").append(date2);
          
          //weather icon
          let day2Icon = fcstResponse.list[15].weather[0].icon;
@@ -119,23 +119,23 @@ $("#searchBtn").on("click", function getWeather(){
          console.log(day2IconURL);
          let day2WeatherIcon = $("<div>");
          day2WeatherIcon.html("<img src='" + day2IconURL + "'>");
-         $("#fiveDayForecast").append(day2WeatherIcon);
+         $("#forecastIcon2").append(day2WeatherIcon);
          //temperature
-         let day2Temperature = $("<div>");
-         var day2Fahrenheit = kelvin2F(fcstResponse.list[15].main.temp);
-         day2Temperature.text(day2Fahrenheit);
-         $("#fiveDayForecast").append(day2Temperature);
+         let day2Temp = $("<div>");
+         var day2F = kelvin2F(fcstResponse.list[15].main.temp);
+         day2Temp.innerHTML = "Temperature: " + day2F + "F";
+        $("#forecastTemp2").append(day2Temp.innerHTML);
  
          //humidity
          let day2Humidity = $("<div>");
-         day2Humidity.text(fcstResponse.list[15].main.humidity);
-         $("#fiveDayForecast").append(day2Humidity);
+        day2Humidity.innerHTML = "Humidity: " + fcstResponse.list[15].main.humidity;
+        $("#forecastHumidity2").append(day2Humidity.innerHTML);
         
          //for day 3
           //date
         let date3 = $("<div>");
         date3.text(fcstResponse.list[23].dt_txt);
-        $("#fiveDayForecast").append(date3);
+        $("#forecastDate3").append(date3);
         
         //weather icon
         let day3Icon = fcstResponse.list[23].weather[0].icon;
@@ -143,23 +143,23 @@ $("#searchBtn").on("click", function getWeather(){
         console.log(day3IconURL);
         let day3WeatherIcon = $("<div>");
         day3WeatherIcon.html("<img src='" + day3IconURL + "'>");
-        $("#fiveDayForecast").append(day3WeatherIcon);
+        $("#forecastIcon3").append(day3WeatherIcon);
         //temperature
-        let day3Temperature = $("<div>");
-        var day3Fahrenheit = kelvin2F(fcstResponse.list[23].main.temp);
-        day3Temperature.text(day3Fahrenheit);
-        $("#fiveDayForecast").append(day3Temperature);
+        let day3Temp = $("<div>");
+         var day3F = kelvin2F(fcstResponse.list[23].main.temp);
+         day3Temp.innerHTML = "Temperature: " + day3F + "F";
+        $("#forecastTemp3").append(day3Temp.innerHTML);
 
         //humidity
         let day3Humidity = $("<div>");
-        day3Humidity.text(fcstResponse.list[23].main.humidity);
-        $("#fiveDayForecast").append(day3Humidity);
+        day3Humidity.innerHTML = "Humidity: " + fcstResponse.list[23].main.humidity;
+        $("#forecastHumidity3").append(day3Humidity.innerHTML);
 
         //for day 4
          //date
          let date4 = $("<div>");
          date4.text(fcstResponse.list[31].dt_txt);
-         $("#fiveDayForecast").append(date4);
+         $("#forecastDate4").append(date4);
          
          //weather icon
          let day4Icon = fcstResponse.list[31].weather[0].icon;
@@ -167,23 +167,23 @@ $("#searchBtn").on("click", function getWeather(){
          console.log(day4IconURL);
          let day4WeatherIcon = $("<div>");
          day4WeatherIcon.html("<img src='" + day4IconURL + "'>");
-         $("#fiveDayForecast").append(day4WeatherIcon);
+         $("#forecastIcon4").append(day4WeatherIcon);
          //temperature
-         let day4Temperature = $("<div>");
-         var day4Fahrenheit = kelvin2F(fcstResponse.list[31].main.temp);
-         day4Temperature.text(day4Fahrenheit);
-         $("#fiveDayForecast").append(day4Temperature);
+         let day4Temp = $("<div>");
+         var day4F = kelvin2F(fcstResponse.list[31].main.temp);
+         day4Temp.innerHTML = "Temperature: " + day4F + "F";
+        $("#forecastTemp4").append(day4Temp.innerHTML);
  
          //humidity
          let day4Humidity = $("<div>");
-         day4Humidity.text(fcstResponse.list[31].main.humidity);
-         $("#fiveDayForecast").append(day4Humidity);
+        day4Humidity.innerHTML = "Humidity: " + fcstResponse.list[31].main.humidity;
+        $("#forecastHumidity4").append(day4Humidity.innerHTML);
 
          //for day 5
           //date
         let date5 = $("<div>");
         date5.text(fcstResponse.list[39].dt_txt);
-        $("#fiveDayForecast").append(date5);
+        $("#forecastDate5").append(date5);
         
         //weather icon
         let day5Icon = fcstResponse.list[39].weather[0].icon;
@@ -191,17 +191,18 @@ $("#searchBtn").on("click", function getWeather(){
         console.log(day5IconURL);
         let day5WeatherIcon = $("<div>");
         day5WeatherIcon.html("<img src='" + day5IconURL + "'>");
-        $("#fiveDayForecast").append(day5WeatherIcon);
+        $("#forecastIcon5").append(day5WeatherIcon);
         //temperature
-        let day5Temperature = $("<div>");
-        var day5Fahrenheit = kelvin2F(fcstResponse.list[39].main.temp);
-        day5Temperature.text(day5Fahrenheit);
-        $("#fiveDayForecast").append(day5Temperature);
+        let day5Temp = $("<div>");
+         var day5F = kelvin2F(fcstResponse.list[39].main.temp);
+         day5Temp.innerHTML = "Temperature: " + day5F + "F";
+        $("#forecastTemp5").append(day5Temp.innerHTML);
 
         //humidity
         let day5Humidity = $("<div>");
-        day5Humidity.text(fcstResponse.list[39].main.humidity);
-        $("#fiveDayForecast").append(day5Humidity);
+        day5Humidity.innerHTML = "Humidity: " + fcstResponse.list[39].main.humidity;
+        $("#forecastHumidity5").append(day5Humidity.innerHTML);
+
 
 
 
